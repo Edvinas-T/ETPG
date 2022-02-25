ab9c2ce20e801a238d4c34d255ff887314e8888a
STUDENT Edvinas Tkacenka <Edvinas.Tkacenka@students.ittralee.ie>
2022-02-25 10:57:37 +0000

    Enemy and Player state machines UI redone(again) a few other things too Signed-off-by: STUDENT Edvinas Tkacenka

--- EdsGame/Assets/Scripts/EnemyStateMachine.cs
+++ EdsGame/Assets/Scripts/EnemyStateMachine.cs
@@ -4,16 +4,76 @@ using UnityEngine;
 
 public class EnemyStateMachine : MonoBehaviour
 {
+    private BattleManager BM;
     public BattleEnemy enemy;
+
+    public enum TurnState
+    {
+        PROCESSING,
+        CHOOSEACTION,
+        WAITING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+
+    private Vector3 startpos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
+        BM = GameObject.Find("BattleManager").GetComponent<BattleManager>();
+        startpos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpdateProgressBar();
+                break;
+
+            case (TurnState.CHOOSEACTION):
+                ChooseAction();
+                currentState = TurnState.WAITING;
+                break;
+
+            case (TurnState.WAITING):
+
+                break;
+
+            case (TurnState.ACTION):
+
+                break;
+
+            case (TurnState.DEAD):
+
+                break;
+
+        }
+        void UpdateProgressBar()
+        {
+            cur_cooldown = cur_cooldown + Time.deltaTime;
+         
+            
+            if (cur_cooldown >= max_cooldown)
+            {
+                currentState = TurnState.CHOOSEACTION;
+            }
+        }
+
+        void ChooseAction()
+        {
+            HandleTurns myAttack = new HandleTurns();
+            myAttack.Attacker = enemy.name;
+            myAttack.AttackersGameObject = this.gameObject;
+            myAttack.AttackersTarget = BM.PlayersInBattle[Random.Range(0, BM.PlayersInBattle.Count)];
+            BM.CollectActions(myAttack);
+        }
     }
 }
--- EdsGame/Assets/Scripts/HeroStateMachine.cs
+++ EdsGame/Assets/Scripts/HeroStateMachine.cs
@@ -1,21 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroStateMachine : MonoBehaviour
 {
 
     public BattlePlayer player;
     
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+
+    public TurnState currentState;
+
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    public Image ProgressBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case (TurnState.PROCESSING):
+                UpdateProgressBar();
+                break;
+
+            case (TurnState.ADDTOLIST):
+                
+                break;
+
+            case (TurnState.WAITING):
+               
+                break;
+
+            case (TurnState.SELECTING):
+             
+                break;
+
+            case (TurnState.ACTION):
+              
+                break;
+
+            case (TurnState.DEAD):
+
+                break;
+
+        }
+
+        void UpdateProgressBar()
+        {
+            cur_cooldown = cur_cooldown + Time.deltaTime;
+            float calc_cooldown = cur_cooldown / max_cooldown;
+            ProgressBar.transform.localScale = new Vector3(Mathf.Clamp(calc_cooldown, 0, 1), 
+                ProgressBar.transform.localScale.y, ProgressBar.transform.localScale.z);
+
+                if (cur_cooldown >= max_cooldown)
+            {
+                currentState = TurnState.ADDTOLIST;
+            }
+        }
         
     }
 }
